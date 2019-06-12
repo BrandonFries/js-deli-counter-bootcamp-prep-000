acede0a3840591762a1b226694ef5e766cdcb0e1
Brandon Fries <brandonfries1993@gmail.com>
2019-06-12 19:52:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ function takeANumber(katzDeliLine, customername) {
   katzDeliLine.push(customername);
   return "Welcome, ${customername}. You are number ${katzDeliLine.lenght} in line."
 }
-function nowServing(katzDeliLine) ()
+function nowServing(katzDeliLine) {}
