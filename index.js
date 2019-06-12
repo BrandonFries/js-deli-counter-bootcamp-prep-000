84a1c0d42bfa49498832686667a95c8028034334
Brandon Fries <brandonfries1993@gmail.com>
2019-06-12 19:52:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,4 @@ function takeANumber(katzDeliLine, customername) {
   katzDeliLine.push(customername);
   return "Welcome, ${customername}. You are number ${katzDeliLine.lenght} in line."
 }
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length == 0)
-  return "There is nobody waiting to be served!"
-  return "Currently serving $katzDeliLine.shift()}."
-}
-}
+function nowServing(katzDeliLine) ()
